a767977c7cc9d12a57b2875c05d49dae0b9ced61
kellachittiharsha-collab <kellachittiharsha@gmail.com>
2026-04-07 18:53:07 +0530

    Create I\o.c

---
+++ WEEK-1/I\o.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int age;
+    printf("REGD NO: 25331A05D0\n");
+    printf("Enter the age of harsha:");
+    scanf("%d",&age);
+    printf("The age of the harsha is %d years old",age);
+    
+    return 0;
+}
