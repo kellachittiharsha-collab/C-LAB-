412e4f950719d2108ba098984e78d6cdab2a41aa
kellachittiharsha-collab <kellachittiharsha@gmail.com>
2026-04-07 18:54:47 +0530

    Create max.c

---
+++ WEEK-2/max.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+ {
+  int a,b,c,max;
+  printf("REGD NO: 25331A05D0\n");
+  printf("Enter the 3 numbers:");
+  scanf("%d %d %d", &a,&b,&c);
+  max=(a>b&&a>c)?a:(b>c?b:c);
+  printf("maximum number is :%d" , max);
+  
+  return 0;
+ }
